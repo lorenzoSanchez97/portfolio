184830d4990ff53ed9edf1093276509d75fb07f3
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2024-11-11 19:03:00 -0300

    margin added to large screens

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import HomeButtonsGroup from "../components/HomeButtonsGroup";
 export default function Home() {
   return (
     <main className="h-[calc(100vh-150px)]">
-      <div className="flex flex-col items-center md:flex-row justify-evenly h-full xl:mx-24">
+      <div className="flex flex-col items-center md:flex-row justify-evenly h-full xl:mx-32">
         <PersonalInformation />
         <HomeButtonsGroup />
       </div>
