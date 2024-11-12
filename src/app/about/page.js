32c2521a918b7dc51bca0b50854aabcea75c5226
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2024-11-12 18:57:50 -0300

    about structure

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+export default function About() {
+    return (
+        <main className="h-full w-full mx-auto flex justify-center items-center">
+            <section className="h-[98%] w-[80%] grid grid-cols-12 gap-x-2">
+                <section className="bg-light-grey w-full h-full rounded col-span-3"></section>
+                <section className="bg-light-grey w-full h-full rounded col-span-6"></section>
+                <section className="bg-light-grey w-full h-full rounded col-span-3"></section>
+            </section>
+        </main>
+    );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -4,11 +4,11 @@ import HomeButtonsGroup from "../components/HomeButtonsGroup";
 
 export default function Home() {
   return (
-    <main className="h-[calc(100vh-250px)] md:h-[calc(100vh-150px)]">
-      <div className="flex flex-col items-center md:flex-row justify-evenly h-full xl:mx-32">
+    <main className="h-[calc(100%-250px)] md:h-[calc(100%-150px)]">
+      <section className="flex flex-col items-center md:flex-row justify-evenly h-full xl:mx-32">
         <PersonalInformation />
         <HomeButtonsGroup />
-      </div>
+      </section>
       <TechsBar />
     </main>
   );
