bfb55f15d1cc096d27d781b283a0afb425b63d7f
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2024-11-17 01:13:26 -0300

    technology stack componente simplified

--- src/components/TechnologyStack.js
+++ src/components/TechnologyStack.js
@@ -1,27 +1,31 @@
 import TechCard from "./TechCard";
 
-export default function TechsBar() {
+const techs = [
+  "api",
+  "bootstrap",
+  "css",
+  "ejs",
+  "express",
+  "figma",
+  "github",
+  "html",
+  "javascript",
+  "mysql",
+  "n8n",
+  "next",
+  "node",
+  "react",
+  "sequelize",
+  "sql",
+  "tailwind"
+];
+
+export default function TechnologyStack() {
   return (
     <section className="flex flex-col items-center text-bright-white text-2xl">
       <h1>Technology Stack</h1>
       <div className="flex flex-wrap gap-y-2 gap-x-2 w-[80%] mt-8">
-        <TechCard techName="api" />
-        <TechCard techName="bootstrap" />
-        <TechCard techName="css" />
-        <TechCard techName="ejs" />
-        <TechCard techName="express" />
-        <TechCard techName="figma" />
-        <TechCard techName="github" />
-        <TechCard techName="html" />
-        <TechCard techName="javascript" />
-        <TechCard techName="mysql" />
-        <TechCard techName="n8n" />
-        <TechCard techName="next" />
-        <TechCard techName="node" />
-        <TechCard techName="react" />
-        <TechCard techName="sequelize" />
-        <TechCard techName="sql" />
-        <TechCard techName="tailwind" />
+        {techs.map((tech, index) => <TechCard techName={tech} key={index} />)}
       </div>
     </section>
   )
