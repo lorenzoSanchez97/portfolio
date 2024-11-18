7d19989a6020e9ccd20b9b06a3398db3edc9bb7a
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2024-11-18 16:00:38 -0300

    pdf download

--- src/components/HomeButton.js
+++ src/components/HomeButton.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 import styles from "./HomeButton.module.css"
 
-export default function HomeButton({ text }) {
+export default function HomeButton({ text, href }) {
     return (
-        <Link href="/about" className={`${styles.button} text-light-blue font-bold text-l no-underline bg-dark-grey py-3 px-20 sm:px-24 md:px-20 lg:px-24 border-solid border-[2px] rounded-lg border-light-blue transition-all duration-300`}>
+        <Link href={href} className={`${styles.button} text-light-blue font-bold text-l no-underline bg-dark-grey py-3 px-20 sm:px-24 md:px-20 lg:px-24 border-solid border-[2px] rounded-lg border-light-blue transition-all duration-300`}>
             {text}
         </Link>
     )
--- src/components/HomeButtonsGroup.js
+++ src/components/HomeButtonsGroup.js
@@ -1,10 +1,13 @@
 import HomeButton from "./HomeButton"
+import styles from "./HomeButton.module.css"
 
 export default function HomeButtonsGroup() {
     return (
         <section className="flex flex-col text-center gap-y-4 px-4 md:px-0 animate-show-in">
-            <HomeButton text={"About"} />
-            <HomeButton text={"Download CV"} />
+            <HomeButton text={"About"} href={"/about"} />
+            <a href="/cv-lorenzo-sanchez.pdf" download="CV - Lorenzo Sanchez.pdf" className={`${styles.button} text-light-blue font-bold text-l no-underline bg-dark-grey py-3 px-20 sm:px-24 md:px-20 lg:px-24 border-solid border-[2px] rounded-lg border-light-blue transition-all duration-300`}>
+                Download CV
+            </a>
         </section>
     )
 }
