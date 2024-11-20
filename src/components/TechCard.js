75827c1d36ffdefa98e34b2b65a0dbd657cdaf51
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2024-11-20 18:46:08 -0300

    font size adjusted

--- src/components/TechCard.js
+++ src/components/TechCard.js
@@ -5,7 +5,7 @@ export default function TechCard({ techName, isHighlighted  }) {
     return (
         <article className={`flex w-fit items-center gap-x-2 text-bright-white text-base px-2 py-1 rounded-md transition hover:cursor-pointer hover:bg-dark-grey ${isHighlighted ? "bg-dark-grey" : "bg-grey-4"}`}>
             <Image src={techsData[techName].icon} alt={`${techsData[techName].name} Icon`} />
-            <span className="font-medium">{techsData[techName].name}</span>
+            <span className="font-normal text-sm">{techsData[techName].name}</span>
         </article>
     )
 }
