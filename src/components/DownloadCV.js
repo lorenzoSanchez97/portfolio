9fe42491400e19a47c4eb04745ce506b6c92f2b7
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2024-11-21 19:13:49 -0300

    download cv button improved

--- src/components/DownloadCV.js
+++ src/components/DownloadCV.js
@@ -1,10 +1,13 @@
+import Image from "next/image"
+import downloadLogo from "@/public/icons/download-icon.svg"
+
 export default function DownloadCV() {
     return (
         <section className="flex flex-col text-white">
             <h1 className="text-light-blue text-2xl mt-2">Curriculum</h1>
             <p className="text-white mt-2 leading-6 text-sm">Debajo, puedes descargar mi CV en formato PDF.</p>
-            <a href="/cv-lorenzo-sanchez.pdf" download="CV - Lorenzo Sanchez.pdf" className="font-medium text-sm hover:cursor-pointer no-underline text-light-blue border-solid border-[2px] py-[5px] w-[50%] mx-auto text-center rounded mt-4">
-                Download CV
+            <a href="/cv-lorenzo-sanchez.pdf" download="CV - Lorenzo Sanchez.pdf" className="hover:scale-[1.05] transition-transform hover:cursor-pointer py-[5px] px-6 mx-auto text-center rounded mt-4">
+               <Image src={downloadLogo} alt="Download logo"/>
             </a>
         </section>
     )
--- src/components/TechCard.js
+++ src/components/TechCard.js
@@ -1,11 +1,11 @@
-import Image from "next/image"
-import { techsData } from "./data/techsData"
+import Image from "next/image";
+import { techsData } from "./data/techsData";
 
-export default function TechCard({ techName, isHighlighted  }) {
+export default function TechCard({ techName, isHighlighted }) {
     return (
-        <article className={`flex w-fit items-center gap-x-2 text-light-blue text-base px-2 py-1 rounded-md transition hover:cursor-pointer hover:bg-grey-4 border-solid border-light-blue border-[2px] ${isHighlighted ? "bg-grey-4" : ""}`}>
+        <article className={`flex w-fit items-center gap-x-2 text-light-blue text-base px-2 py-1 rounded-md transition-transform hover:cursor-pointer hover:scale-[1.02] border-solid border-light-blue border-[2px] ${isHighlighted ? "scale-105" : ""}`}>
             <Image src={techsData[techName].icon} alt={`${techsData[techName].name} Icon`} />
             <span className="font-medium text-sm">{techsData[techName].name}</span>
         </article>
-    )
+    );
 }
