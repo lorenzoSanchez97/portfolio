83451cd3cae82050e959d7de84d1b139015f66e1
Lorenzo Sanchez <lorenzo97sz@gmail.com>
2025-02-28 16:06:10 -0300

    cv updated

--- src/components/DownloadCV.js
+++ src/components/DownloadCV.js
@@ -6,7 +6,7 @@ export default function DownloadCV() {
         <section className="flex flex-col text-white">
             <h1 className="text-light-blue text-2xl mt-2">Curriculum</h1>
             <p className="text-white mt-2 leading-6 text-sm">Debajo, puedes descargar mi CV en formato PDF.</p>
-            <a aria-label="descargar curriculum" href="/cv-lorenzo-sanchez.pdf" download="CV - Lorenzo Sanchez.pdf" className="hover:scale-[1.08] transition-transform hover:cursor-pointer py-[5px] px-6 mx-auto text-center rounded mt-4">
+            <a aria-label="descargar curriculum" href="/CV-Lorenzo_Sanchez-Desarrollador_Web.pdf" download="CV - Lorenzo Sanchez.pdf" className="hover:scale-[1.08] transition-transform hover:cursor-pointer py-[5px] px-6 mx-auto text-center rounded mt-4">
                <Image src={downloadLogo} alt="Descargar CV"/>
             </a>
         </section>
